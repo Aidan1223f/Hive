321d06e8f780209af2895e3d7497b8992510cba2
Aidan1223f <112412969+Aidan1223f@users.noreply.github.com>
2023-08-01 12:18:38 -0700

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+ 
+module.exports = {
+    // Your other Next.js configurations go here
+    experimental: {
+      optimizeFonts: true,
+    },
+  };
+  
+module.exports = nextConfig
+
